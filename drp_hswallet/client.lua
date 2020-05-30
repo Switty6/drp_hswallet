df64ce1742a245d08a891db7d0332f6d3b0b413e
Switty <42062456+Switty6@users.noreply.github.com>
2020-05-30 11:14:15 +0300

    Yeet. Now use /wallet

--- drp_hswallet/client.lua
+++ drp_hswallet/client.lua
@@ -1,51 +1,29 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118 
-}
+local status = false
 
-local Key = Keys["Z"] -- set your key
-
-local headshot
-Citizen.CreateThread(function()
-    local status = false
-    while true do
+RegisterCommand("wallet", function()
+    status = not status
+    local spawnedIn = false
+    local head = RegisterPedheadshot(PlayerPedId())
+            
+    while not IsPedheadshotReady(head) or not IsPedheadshotValid(head) do
         Wait(1)
-        local spawnedIn = false
-        if exports["drp_id"]:SpawnedInAndLoaded() and not spawnedIn then
-            spawnedIn = true
-            if IsControlJustReleased(0, Key) then
-                status = not status
-                
-                local head = RegisterPedheadshot(PlayerPedId())
-    
-                while not IsPedheadshotReady(head) or not IsPedheadshotValid(head) do
-                    Wait(1)
-                end
-                
-                SendNUIMessage({
-                    type = 'key',
-                    status = status,
-                    headshot = headshot,
-                    resourceName = GetCurrentResourceName()
-                })
-                Wait(460)
-                headshot = GetPedheadshotTxdString(head)    
-            end
-    
-            if status then    
-                DrawSprite(headshot, headshot, 0.7345, 0.83, 0.05, 0.08, 0.0, 255, 255, 255, 255)                
-            end
-        end
-
     end
-    
+            
+    SendNUIMessage({
+        type = 'key',
+        status = status,
+        headshot = headshot,
+        resourceName = GetCurrentResourceName()
+    })
+
+    Wait(460)
+    headshot = GetPedheadshotTxdString(head)
+
+        
+    while status do
+        Wait(1)
+        DrawSprite(headshot, headshot, 0.7345, 0.83, 0.05, 0.08, 0.0, 255, 255, 255, 255)
+    end
 end)
 
 RegisterNUICallback("check", function(data)
@@ -54,12 +32,19 @@ end)
 
 RegisterNetEvent('sendData')
 AddEventHandler('sendData', function(name, id, cash, dirty)
+    local cash = cash + 0.0
+    if dirty == nil then
+        dirty = 0.0
+    else 
+        local dirty = dirty + 0.0
+    end
+
     SendNUIMessage({
         type = 'walletdata',
-        name = name, 
+        name = name,
         id = id,
         cash = format_thousand(cash),
-        dirty = format_thousand(dirty),
+        dirty = format_thousand(dirty)
     })
 end)
 
@@ -68,5 +53,5 @@ function format_thousand(v)
     local pos = string.len(s) % 3
     if pos == 0 then pos = 3 end
     return string.sub(s, 1, pos)
-    ..string.gsub(string.sub(s, pos+1), "(...)", ".%1")
+        .. string.gsub(string.sub(s, pos + 1), "(...)", ".%1")
 end
